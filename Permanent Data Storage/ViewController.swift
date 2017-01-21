46be72f2962d6e5fb271912d0c024338232d35b9
TM <Torben@Torbens-MacBook-Air.local>
2017-01-21 19:41:08 +0100

    Nr 2

--- Permanent Data Storage/ViewController.swift
+++ Permanent Data Storage/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        UserDefaults.standard.set("Torben", forKey: "name")
+        UserDefaults.standard.set("Torben Mæhle", forKey: "name")
         
         let nameObject = UserDefaults.standard.object(forKey: "name")
         
